039e57fb5cbb9b5fd6ab4bbafb08fc6507b1a567
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-03-17 19:35:26 +0530

    yt

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
 //your JS code here. If required.
-function getObject() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const person = {
-          age: Number(document.getElementById('age').value),
-          name: document.getElementById('name').value
-        };
-        if (person.age >= 18) {
-          resolve(person);
-        } else {
-          reject(person);
-        }
-      }, 4000);
-    });
-  }
-  
-  document.getElementById('btn').addEventListener('click', (event) => {
+const form = document.querySelector('form');
+  const ageInput = document.getElementById('age');
+  const nameInput = document.getElementById('name');
+  const submitButton = document.getElementById('btn');
+
+  form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const ageInput = document.getElementById('age');
-    const nameInput = document.getElementById('name');
-    const age = Number(ageInput.value);
-    const name = nameInput.value;
+    const age = ageInput.value.trim();
+    const name = nameInput.value.trim();
+
     if (age === '' || name === '') {
       alert('Please fill in both fields.');
       return;
     }
-    getObject()
-      .then((person) => {
-        const extractedValue = person.age;
-        const newObject = { extractedValue };
-        alert(`Welcome, ${person.name}. You can vote.`);
-        return newObject;
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve(name);
+        } else {
+          reject(name);
+        }
+      }, 4000);
+    });
+
+    promise
+      .then((name) => {
+        alert(`Welcome, ${name}. You can vote.`);
       })
-      .then((finalObject) => {
-        // Do something with the final object
-        console.log(finalObject);
-      })
-      .catch((person) => {
-        alert(`Oh sorry ${person.name}. You aren't old enough.`);
+      .catch((name) => {
+        alert(`Oh sorry ${name}. You aren't old enough.`);
       });
   });
